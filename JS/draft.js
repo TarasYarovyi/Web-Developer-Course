2c0c88c264637724b6cc413a6490e59e3c3309f5
TarasYarovyi <yarovoy.taras@gmail.com>
2023-10-26 23:00:10 +0200

    11.180

--- JS/draft.js
+++ JS/draft.js
@@ -1,6 +1,8 @@
-let multiply = function (a, b) {
-  return a * b;
-};
-console.log(multiply(3, 6));
-let test = multiply;
-console.log(test(10, 25));
+function mutiplyArr(arr, multiplier) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] *= multiplier;
+  }
+}
+const tab = [1, 2, 3, 4, 5, 6, 7];
+mutiplyArr(tab, 3);
+console.log(tab);
