cf1140415cca17b18c903743b7b19e750602a22a
TarasYarovyi <yarovoy.taras@gmail.com>
2023-10-26 23:33:54 +0200

    11.183

--- JS/draft.js
+++ JS/draft.js
@@ -1,8 +1,15 @@
-function mutiplyArr(arr, multiplier) {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] *= multiplier;
+function createUser(name, contact) {
+  const user = {
+    name: name,
+  };
+  if (typeof contact === "string") {
+    user.email = contact;
+  } else if (typeof contact === "number") {
+    user.tel = contact;
   }
+  return user;
 }
-const tab = [1, 2, 3, 4, 5, 6, 7];
-mutiplyArr(tab, 3);
-console.log(tab);
+const user1 = createUser("Ola", "ola@example.com");
+const user2 = createUser("Kasia", 782256535);
+console.log(user1);
+console.log(user2);
