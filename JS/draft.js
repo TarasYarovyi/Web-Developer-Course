b8b68434efff18822262d86e0e77ce525e50b3a7
TarasYarovyi <yarovoy.taras@gmail.com>
2023-11-06 17:30:36 +0100

    12.213.34

--- JS/draft.js
+++ JS/draft.js
@@ -1,27 +1,14 @@
-let sampleObj = {
-  name: "John",
-  age: 25,
-  isStudent: true,
-};
-
-function enumeratePropertiesUsingEntries(obj) {
-  return Object.entries(obj).map(([key, value]) => ({
-    propertyName: key,
-    dataType: typeof value,
-  }));
+function addPropertyUsingDefineProperty(
+  obj,
+  propName,
+  propValue,
+  writable = false,
+  enumerable = false
+) {
+  Object.defineProperty(obj, propName, {
+    value: propValue,
+    writable: writable,
+    enumerable: enumerable,
+  });
+  return obj;
 }
-
-// let sampleObj = {
-//   name: "John",
-//   age: 25,
-//   isStudent: true
-// };
-
-// function enumeratePropertiesUsingEntries(obj) {
-//   const entries = Object.entries(obj);
-//   const result = []
-//   for(const [key, value] of entries){
-//       result.push({propertyName: key, dataType: typeof value});
-//   }
-//   return result;
-// }
