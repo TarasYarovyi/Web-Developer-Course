642ff5bbab709bbd39a4f9da2f8cb38117159205
TarasYarovyi <yarovoy.taras@gmail.com>
2023-11-06 17:17:20 +0100

    12.211.33

--- JS/draft.js
+++ JS/draft.js
@@ -1,3 +1,27 @@
-function mergeObjects(...objects) {
-  return Object.assign({}, ...objects);
+let sampleObj = {
+  name: "John",
+  age: 25,
+  isStudent: true,
+};
+
+function enumeratePropertiesUsingEntries(obj) {
+  return Object.entries(obj).map(([key, value]) => ({
+    propertyName: key,
+    dataType: typeof value,
+  }));
 }
+
+// let sampleObj = {
+//   name: "John",
+//   age: 25,
+//   isStudent: true
+// };
+
+// function enumeratePropertiesUsingEntries(obj) {
+//   const entries = Object.entries(obj);
+//   const result = []
+//   for(const [key, value] of entries){
+//       result.push({propertyName: key, dataType: typeof value});
+//   }
+//   return result;
+// }
