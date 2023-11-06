beb90d13fe377f578b316233a6ccec3dd02e1270
TarasYarovyi <yarovoy.taras@gmail.com>
2023-11-06 17:00:31 +0100

    12.210.32

--- JS/draft.js
+++ JS/draft.js
@@ -1,14 +1,3 @@
-let users = [
-  { name: "John", age: 25, favoriteColor: "blue" },
-  { name: "Jane", age: 30, favoriteColor: "red" },
-  { name: "Doe", age: 20, favoriteColor: "green" },
-  { name: "Anna", age: 17, favoriteColor: "yellow" },
-  { name: "Mike", age: 30, favoriteColor: "pink" },
-  { name: "Chris", age: 20, favoriteColor: "purple" },
-  { name: "Eva", age: 25, favoriteColor: "orange" },
-  { name: "Max", age: 30, favoriteColor: "brown" },
-];
-
-function areAllAdults(arr) {
-  return arr.every((el) => el.age >= 18);
+function mergeObjects(...objects) {
+  return Object.assign({}, ...objects);
 }
