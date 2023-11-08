55ace75585aed7c07888002d81daa2a32df2e29c
TarasYarovyi <yarovoy.taras@gmail.com>
2023-11-08 19:42:26 +0100

    12.212.40

--- JS/draft.js
+++ JS/draft.js
@@ -1,8 +1,31 @@
-function add(x) {
-  return function (y) {
-    return x + y;
-  };
+const test = {
+  property1: "value1",
+  property2: "value2",
+  property3: "value3",
+};
+
+function serializeObject(obj) {
+  const entries = Object.entries(obj);
+  const result = [];
+  for (let [key, value] of entries) {
+    result.push(key + ": " + value);
+  }
+  return result.join(" | ");
 }
 
-const result = add(5);
-console.log(result(10));
+function deserializeString(str) {
+  const arr = str.split(" | ");
+  const obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].split(": ");
+  }
+  for (let [key, value] of arr) {
+    obj[key] = value;
+  }
+  return obj;
+}
+serializeObject(test);
+const string = "property1: value1 | property2: value2 | property3: value3";
+
+console.log(deserializeString(string));
+console.log(Object.entries(test));
