075abd5759e6090a5e9d66e6ec4a9ac044467a25
TarasYarovyi <yarovoy.taras@gmail.com>
2023-11-08 20:43:58 +0100

    13.228

--- JS/draft.js
+++ JS/draft.js
@@ -1,8 +1,15 @@
-const url = "https://swapi.dev/api/people/2/";
+const url = "https://v2.jokeapi.dev/joke/Any";
 
 fetch(url)
   .then((response) => response.json())
-  .then((result) => showData(result));
+  .then((result) => {
+    if (result.hasOwnProperty("setup")) {
+      console.log(result.setup);
+      setTimeout(() => {
+        console.log(result.delivery);
+      }, 3000);
+    }
+  });
 // or .then(showData);
 
 function showData(data) {
