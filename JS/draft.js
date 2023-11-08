f10b20a857372f579c4d7867155fb58ef1f7845e
TarasYarovyi <yarovoy.taras@gmail.com>
2023-11-08 20:32:08 +0100

    13.227

--- JS/draft.js
+++ JS/draft.js
@@ -1,31 +1,10 @@
-const test = {
-  property1: "value1",
-  property2: "value2",
-  property3: "value3",
-};
+const url = "https://swapi.dev/api/people/2/";
 
-function serializeObject(obj) {
-  const entries = Object.entries(obj);
-  const result = [];
-  for (let [key, value] of entries) {
-    result.push(key + ": " + value);
-  }
-  return result.join(" | ");
+fetch(url)
+  .then((response) => response.json())
+  .then((result) => showData(result));
+// or .then(showData);
+
+function showData(data) {
+  console.log(data);
 }
-
-function deserializeString(str) {
-  const arr = str.split(" | ");
-  const obj = {};
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i].split(": ");
-  }
-  for (let [key, value] of arr) {
-    obj[key] = value;
-  }
-  return obj;
-}
-serializeObject(test);
-const string = "property1: value1 | property2: value2 | property3: value3";
-
-console.log(deserializeString(string));
-console.log(Object.entries(test));
