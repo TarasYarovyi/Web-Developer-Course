7b35e3b5ea962e81ffd4b5521044ecc75151dcd4
TarasYarovyi <yarovoy.taras@gmail.com>
2023-11-08 21:07:53 +0100

    13.229

--- JS/draft.js
+++ JS/draft.js
@@ -1,17 +1,24 @@
 const url = "https://v2.jokeapi.dev/joke/Any";
 
-fetch(url)
-  .then((response) => response.json())
-  .then((result) => {
-    if (result.hasOwnProperty("setup")) {
-      console.log(result.setup);
-      setTimeout(() => {
-        console.log(result.delivery);
-      }, 3000);
-    }
-  });
-// or .then(showData);
+window.onload = function () {
+  const div = document.createElement("div");
+  document.querySelector("body").appendChild(div);
+  function getJoke() {
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.hasOwnProperty("setup")) {
+          div.textContent = result.setup;
+          setTimeout(() => {
+            div.textContent = result.delivery;
+          }, 3000);
+        } else div.textContent = result.joke;
+      });
+    // or .then(showData);
 
-function showData(data) {
-  console.log(data);
-}
+    function showData(data) {
+      console.log(data);
+    }
+  }
+  setInterval(getJoke, 5000);
+};
