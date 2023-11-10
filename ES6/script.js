72102f7a80693b9a035b1501d20655da654b4548
TarasYarovyi <yarovoy.taras@gmail.com>
2023-11-10 22:03:32 +0100

    14.239

--- ES6/script.js
+++ ES6/script.js
@@ -1,23 +1,41 @@
-// destructurization
-const employee = {
-  name: "Ola",
-  id: 34,
-  employment: {
-    company: "example.com",
-    address: {
-      country: "PL",
-      city: "WRO",
-    },
-  },
+class Car {
+  constructor(brand, color) {
+    this.brand = brand;
+    this.color = color;
+  }
+  printData() {
+    console.log(this.brand);
+    console.log(this.color);
+  }
+}
+
+const bmw = new Car("BMW", "red");
+bmw.printData();
+
+let car2 = class {
+  constructor(brand, color) {
+    this.brand = brand;
+    this.color = color;
+    this._age = 1;
+  }
+  printData() {
+    console.log(this.brand);
+    console.log(this.color);
+  }
+
+  set age(value) {
+    if (value > 0) return this._age;
+  }
+  get age() {
+    return this._age;
+  }
+  static getDefaultCar() {
+    return new car2();
+  }
 };
 
-const {
-  name,
-  employment: { company },
-  employment: {
-    address: { city },
-  },
-} = employee;
-console.log(name);
-console.log(company);
-console.log(city);
+const audi = new car2("Audi", "white");
+audi.printData();
+car2.age = 2;
+console.log(car2.age);
+console.log(car2.getDefaultCar());
