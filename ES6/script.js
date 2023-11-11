22af6063b2649fd89726ab01c137260723b1bd8f
TarasYarovyi <yarovoy.taras@gmail.com>
2023-11-11 11:36:55 +0100

    12.197

--- ES6/script.js
+++ ES6/script.js
@@ -1,43 +1,56 @@
-class Car {
-  constructor(brand, color) {
-    this.brand = brand;
-    this.color = color;
-    this._age = 1;
-    //static property emulation
-    if (Car.countInstances === undefined) {
-      Car.countInstances = 0;
-    }
-    Car.countInstances++;
-  }
-  printData() {
-    console.log(this.brand);
-    console.log(this.color);
-  }
-  //or an arrow, but with a difference
-  //   printData = () => {
-  //     console.log(this.brand);
-  //     console.log(this.color);
-  //   };
-
+let car = {
+  brand: "audi",
+  color: "red",
   set age(value) {
     if (value > 0) {
       this._age = value;
     } else {
-      console.log("Wrong age");
+      console.log("False age");
     }
-  }
+  },
   get age() {
     return this._age;
-  }
-  static getDefaultCar() {
-    return new Car();
-  }
-  static getNumbersOfInstances() {
-    return Car.countInstances;
-  }
+  },
+};
+console.log(car);
+
+for (key in car) {
+  console.log(`${key}: ${car[key]}`); //brand: car, color: white, age: 1
 }
 
-//static methods, "static" properties of classes
-console.log(Car.getNumbersOfInstances());
-const bmw = new Car();
-console.log(Car.getNumbersOfInstances());
+console.log(Object.keys(car)); //(3) ['brand', 'color', 'age']
+
+const { brand, color } = car;
+console.log(brand, color); //car red 1
+
+const engine = "V8";
+const engineObj = { engine };
+
+// for (key in engineObj) {
+//   if (engineObj.hasOwnProperty(key)) {
+//     car[key] = engineObj[key];
+//   }
+// }
+//or
+// car = { ...car, ...engineObj };
+//or
+Object.assign(car, engineObj);
+
+car.age = 3;
+console.log(car.age); //3
+car.age = -1; //Wrong age
+console.log(car.age); //still 3 becouse of setter
+console.log(car);
+
+const myObj = {
+  data: "abc",
+  loggerA: () => {
+    console.log(this);
+  },
+  loggerB() {
+    console.log(this);
+  },
+};
+
+myObj.loggerA(); // undefined becouse "this" loggerA = window
+myObj.loggerB(); // 'abc' becouse "this" loggerA = myObj
