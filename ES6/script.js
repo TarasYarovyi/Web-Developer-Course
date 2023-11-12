82122b1b83d695661e71321803bd2c2cec69d91f
TarasYarovyi <yarovoy.taras@gmail.com>
2023-11-12 10:53:04 +0100

    14.240

--- ES6/script.js
+++ ES6/script.js
@@ -1,56 +1,26 @@
-let car = {
-  brand: "audi",
-  color: "red",
-  set age(value) {
-    if (value > 0) {
-      this._age = value;
+class Animal {
+  constructor(name) {
+    if (typeof name === "string") {
+      this._name = name;
     } else {
-      console.log("False age");
+      console.log("Wrong name");
     }
-  },
-  get age() {
-    return this._age;
-  },
-};
-console.log(car);
-
-for (key in car) {
-  console.log(`${key}: ${car[key]}`); //brand: car, color: white, age: 1
+    if (Animal.count === undefined) Animal.count = 0;
+    this.id = Animal.count;
+    Animal.count++;
+  }
+  set name(value) {
+    if (typeof value === "string") {
+      this._name = value;
+    } else {
+      console.log("Wrong name");
+    }
+  }
+  get name() {
+    return this._name;
+  }
 }
 
-console.log(Object.keys(car)); //(3) ['brand', 'color', 'age']
-
-const { brand, color } = car;
-console.log(brand, color); //car red 1
-
-const engine = "V8";
-const engineObj = { engine };
-
-// for (key in engineObj) {
-//   if (engineObj.hasOwnProperty(key)) {
-//     car[key] = engineObj[key];
-//   }
-// }
-//or
-// car = { ...car, ...engineObj };
-//or
-Object.assign(car, engineObj);
-
-car.age = 3;
-console.log(car.age); //3
-car.age = -1; //Wrong age
-console.log(car.age); //still 3 becouse of setter
-console.log(car);
-
-const myObj = {
-  data: "abc",
-  loggerA: () => {
-    console.log(this);
-  },
-  loggerB() {
-    console.log(this);
-  },
-};
-
-myObj.loggerA(); // undefined becouse "this" loggerA = window
-myObj.loggerB(); // 'abc' becouse "this" loggerA = myObj
+const cat = new Animal("Barsik");
+cat.name = 1;
+console.log(cat.name);
