c182e48dcb40835361092117a78813c339b03263
TarasYarovyi <yarovoy.taras@gmail.com>
2023-11-22 23:12:39 +0100

    15.261

--- Patterns/script.js
+++ Patterns/script.js
@@ -1,23 +1,27 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+class AppConfig {
+  constructor() {
+    if (typeof AppConfig.instance === "object") {
+      return AppConfig.instance;
+    }
+
+    this.config = {};
+
+    AppConfig.instance = this;
+    return this;
+  }
+
+  setConfig(key, value) {
+    this.config[key] = value;
+  }
+
+  getConfig(key) {
+    return this.config[key];
+  }
+
+  static getInstance() {
+    if (!this._instance) {
+      this._instance = new AppConfig();
+    }
+    return this._instance;
+  }
 }
-Person.prototype.introduce = function () {
-  return `Hello, my name is ${this.name} and I am ${this.age} years old`;
-};
-
-function Employee(name, age, jobTitle) {
-  Person.call(this, name, age);
-  this.jobTitle = jobTitle;
-}
-Employee.prototype = new Person(); //or Object.create(Person.prototype)
-Employee.prototype.whatDoYouDo = function () {
-  return `I work as a ${this.jobTitle}`;
-};
-
-const person1 = new Person("Taras", 35);
-const employee1 = new Employee("Taras", 35, "Manager");
-
-console.log(person1.introduce());
-console.log(employee1.whatDoYouDo());
-console.log(employee1);
