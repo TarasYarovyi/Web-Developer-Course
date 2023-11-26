90dd81607ccdd7a4d1a0a3c0b830131d18cac732
TarasYarovyi <yarovoy.taras@gmail.com>
2023-11-26 17:43:09 +0100

    15.263 (Pattern Factory)

--- Patterns/script.js
+++ Patterns/script.js
@@ -1,18 +1,73 @@
-const Module = (function () {
-  let privData = 99;
-  function getPrivData() {
-    return privData;
-  }
+function Vehicle(name, numWheels) {
+  this.name = name;
+  this.numWheels = numWheels;
+}
 
-  function setPrivData(value) {
-    privData = value;
-  }
+Vehicle.prototype.logInfo = function () {
+  console.log(this.name, "numWheels: " + this.numWheels);
+};
 
+function Bike(name) {
+  Vehicle.call(this, name, 2);
+  this.electricFlag = false;
+  this.isElectric = function () {
+    return this.electricFlag;
+  };
+}
+
+Bike.prototype = new Vehicle();
+Bike.prototype.constructor = Bike;
+
+// let bike1 = new Bike("Kross");
+// console.log(bike1);
+// bike1.logInfo();
+function BikeFactory() {
+  return new Bike("rower");
+}
+
+function Car(name) {
+  Vehicle.call(this, name, 4);
+  this.cartype = "sedan";
+  this.setCarType = function (type) {
+    this.cartype = type;
+  };
+}
+
+Car.prototype = new Vehicle();
+Car.prototype.constructor = Car;
+
+// let car1 = new Car("BMW");
+// console.log(car1);
+// car1.logInfo();
+function CarFactory() {
+  return new Car("samochód");
+}
+
+console.log(CarFactory());
+
+// abstract factory
+
+const AbstractVehicleFactory = (function () {
+  let types = [];
   return {
-    getPrivData: getPrivData,
-    setPrivData: setPrivData,
+    registerType: function (type, factory) {
+      let v = factory();
+      if (v.numWheels && v.name) {
+        types[type] = factory;
+      }
+    },
+    getVehicle: function (type) {
+      let vehicleType = types[type];
+      if (vehicleType) {
+        return vehicleType();
+      } else {
+        return null;
+      }
+    },
   };
 })();
-Module.setPrivData(88);
-console.log(Module.getPrivData());
-console.log(Module.privDatadata);
+
+AbstractVehicleFactory.registerType("bike", BikeFactory);
+AbstractVehicleFactory.registerType("car", CarFactory);
+const car2 = AbstractVehicleFactory.getVehicle("car");
+console.log(car2);
