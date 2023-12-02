554c209d55d3e0973e2689771c245edf06bebeb9
TarasYarovyi <yarovoy.taras@gmail.com>
2023-12-02 14:16:52 +0100

    15.271 (Observer pattern)

--- Patterns/script.js
+++ Patterns/script.js
@@ -1,24 +1,24 @@
-class Coffee {
-  cost() {
-    return 10;
+class Customer {
+  constructor(name) {
+    this.name = name;
+  }
+  notify(product) {
+    console.log(`${this.name} dostał ofertę na ${product}`);
   }
 }
 
-function addMilk(coffee) {
-  const originalCost = coffee.cost();
-  coffee.cost = function () {
-    return originalCost + 2;
-  };
-}
-function addSugar(coffee) {
-  const originalCost = coffee.cost();
-  coffee.cost = function () {
-    return originalCost + 1;
-  };
-}
-function addWhippedCream(coffee) {
-  const originalCost = coffee.cost();
-  coffee.cost = function () {
-    return originalCost + 3;
-  };
+class ProductStore {
+  constructor() {
+    this.observers = [];
+  }
+  subscribe(observer) {
+    this.observers.push(observer);
+  }
+  unsubscribe(observer) {
+    let index = this.observers.findIndex((el) => el === observer);
+    this.observers.splice(index, 1);
+  }
+  addNewProduct(product) {
+    this.observers.forEach((observer) => observer.notify(product));
+  }
 }
