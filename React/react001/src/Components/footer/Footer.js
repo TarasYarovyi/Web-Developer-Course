d709f9b821f4482fb6fb313ff26e087178dbef24
TarasYarovyi <yarovoy.taras@gmail.com>
2024-02-26 21:41:51 +0100

    19.345 (JSX rendering elements with if )

--- React/react001/src/Components/footer/Footer.js
+++ React/react001/src/Components/footer/Footer.js
@@ -1,19 +1,44 @@
 import React from "react";
 import "./footer.css";
 import Copyright from "../copyright/Copyright";
+
+const elements1 = ["TOS", "About"];
+const items = [];
+const elements2 = ["contact", "legal"];
+
 function Footer(props) {
+  for (const [index, value] of elements1.entries()) {
+    items.push(<li>{value}</li>);
+  }
+
+  let isLogged = true;
+  let button;
+  if (isLogged) {
+    button = <button>Logout</button>;
+  } else {
+    button = <button>Log in</button>;
+  }
+
   return (
     <div>
       <footer>
         <ul>
-          <li>Regulamin</li>
-          <li>FAQ</li>
-          <li>O nas</li>
+          {items}
           <li>
             Contact: {props.contactEmail},{props.companyData.city}
           </li>
+          {elements2.map((value, index) => {
+            return <li>{value}</li>;
+          })}
         </ul>
         <Copyright year="2024" />
+        {button}
+        {isLogged === true && <div>Sprawdź pocztę</div>}
+        {isLogged ? (
+          <div>Zobacz notyfikacje</div>
+        ) : (
+          <div>Zaloguj się dla notyfikacji </div>
+        )}
       </footer>
     </div>
   );
