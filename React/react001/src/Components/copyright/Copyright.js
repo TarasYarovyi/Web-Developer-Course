a297eec3c31675467b1acf01b967fcbc514470ce
TarasYarovyi <yarovoy.taras@gmail.com>
2024-02-29 20:31:10 +0100

    19.348 (Sass in React)

--- React/react001/src/Components/copyright/Copyright.js
+++ React/react001/src/Components/copyright/Copyright.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./copyright.css";
 import styles from "./styles.module.css";
+import "./sassStyles.scss";
 
 class Copyright extends React.Component {
   render() {
     return (
-      <div>
+      <div className="copyrightBox">
         <span className={styles.info}>
           Copyright &copy;
           {this.props.year} example.com All rights reserved
