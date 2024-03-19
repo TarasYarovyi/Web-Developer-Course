df2621082608d49955f4e8e43174dfafae4e4735
TarasYarovyi <yarovoy.taras@gmail.com>
2024-03-19 21:14:58 +0100

    20.355 (requests)

---
+++ node/node02_request.js
@@ -0,0 +1,20 @@
+let http = require("http");
+let url = require("url");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write("req.url: " + req.url + "<br>");
+    let parsedUrl = url.parse(req.url, true);
+
+    res.write("parsedUrl.pathname: " + parsedUrl.pathname + "<br>");
+    res.write("parsedUrl.query: " + JSON.stringify(parsedUrl.query) + "<br>");
+    if (parsedUrl.query.arg2) {
+      res.write(
+        "parsedUrl.query.arg2: " + JSON.stringify(parsedUrl.query.arg2)
+      );
+    }
+
+    res.end();
+  })
+  .listen(8080);
